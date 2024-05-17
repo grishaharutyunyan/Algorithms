f60d25c39e056b50308823d44dbea3b0c59e9908
grishaharutyunyan <grishaharutyunian@gmail.com>
2024-05-17 19:19:49 +0400

    *

---
+++ LeetCode/141.Linked List Cycle.js
@@ -0,0 +1,18 @@
+var hasCycle = function(head) {
+    if (!head || !head.next) {
+        return false; 
+    }
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;         
+        fast = fast.next.next;    
+
+        if (slow === fast) {      
+            return true;
+        }
+    }
+
+    return false; 
+};
