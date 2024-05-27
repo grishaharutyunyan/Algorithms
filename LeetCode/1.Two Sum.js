a87831bdb76caad4768e8d01084d0ebf1f0c1980
grishaharutyunyan <grishaharutyunian@gmail.com>
2024-05-27 22:21:40 +0400

    .

---
+++ LeetCode/1.Two Sum.js
@@ -0,0 +1,12 @@
+
+var twoSum = function(nums, target) {
+    let hashMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (hashMap.has(complement)) {
+            return [hashMap.get(complement), i];
+        }
+        hashMap.set(nums[i], i);
+    }
+    return [-1 ,-1];
+};
